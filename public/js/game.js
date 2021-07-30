c7998751f48ee60bf8f940c965be53b92d485869
AmoghTantradi <amogh_t2003@berkeley.edu>
2021-07-30 18:30:32 +0000

    added some functionality to game.js

--- public/js/game.js
+++ public/js/game.js
@@ -1,1 +1,23 @@
 console.log('game script is running!')
+
+let googleUserId
+
+//when the user starts the game, we load up a list of words 
+window.onload = (event) => {
+    firebase.auth().onAuthStateChanged((user) => {
+        if(user){
+            //check if the user has logged in
+            googleUserId = user.uid
+            getWords(googleUserId)
+        }
+        else{
+            //navigate back to the login page
+            window.location = "index.html"
+        }
+    }
+    )
+}
+//gets the words from the database
+const getWords = (userId) => {
+
+}
