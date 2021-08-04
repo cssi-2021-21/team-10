edcbd0e7400c50cc256daa20d76f80512838e46c
NicoleJoseph18 <nicolejosephk64581@gmail.com>
2021-08-04 18:23:39 +0000

    edited game.html

--- public/js/game.js
+++ public/js/game.js
@@ -181,3 +181,24 @@ const onSubmit= (elementId) => {
   }
   updateGameHTML()
 }
+
+
+
+/*
+//timer
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45" />
+    </g>
+  </svg>
+  <span>
+    <!-- Remaining time label -->
+  </span>
+</div>
+`;
+console.log("timer is working")
+
+
+*/
